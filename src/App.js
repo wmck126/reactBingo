7a494cdabe56352a690a7ab6879b75b3e4c83185
Ward McKinley <wmck126@gmail.com>
2023-01-26 11:25:00 -0500

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path="/testing">
+              <h1>Test Route</h1>
+            </Route>
+            <Route path="/" exact>
+              <h1>Page counter</h1>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+      
   );
 }
 
