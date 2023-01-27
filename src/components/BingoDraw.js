e3eae8669818d67d4bd3786800c4fbe89f877235
Ward McKinley <wmck126@gmail.com>
2023-01-27 16:48:57 -0500

    added random selector for drawing

--- src/components/BingoDraw.js
+++ src/components/BingoDraw.js
@@ -1,7 +1,28 @@
 import React from 'react'
 import './BingoDraw.css'
 
-function BingoDraw() {
+function BingoDraw({numbersDrawn}) {
+
+  function randomDrawing(){
+    const randNumber = Math.floor(Math.random() * (76 - 1) + 1)
+    if(numbersDrawn.includes(randNumber) === true){
+      randomDrawing()
+    } else {
+      numbersDrawn.push(randNumber)
+    }
+    
+    if (randNumber >= 1 && randNumber <= 15){
+      alert('B' + randNumber)
+    } else if (randNumber >= 16 && randNumber <=30){
+      alert('I' + randNumber)
+    } else if (randNumber >=31 && randNumber <=45){
+      alert('N' + randNumber)
+    } else if (randNumber >=46 && randNumber <=60){
+      alert('G' + randNumber)
+    } else if (randNumber >=61 && randNumber <=75){
+      alert('O' + randNumber)
+    }
+  }
 
   function setRowNumbers(min, max){
     let row = []
@@ -15,39 +36,42 @@ function BingoDraw() {
   const nRow = setRowNumbers(31, 45)
   const gRow = setRowNumbers(46, 60)
   const oRow = setRowNumbers(61, 75)
-  console.log(bRow)
+
   return (
-    <div id="bingo-list">
-      <div id="B-Row">
-        <h2>B</h2>
-        {bRow.map((x) => (
-          <p id="number">{x}</p>
-        ))}
+    <div id="container">
+      <div id="bingo-list">
+        <div id="B-Row">
+          <h2>B</h2>
+          {bRow.map((x) => (
+            <p className='number' id={x}>{x}</p>
+          ))}
+        </div>
+        <div id="I-Row">
+          <h2>I</h2>
+          {iRow.map((x) => (
+            <p className='number' id={x}>{x}</p>
+          ))}
+        </div>
+        <div id="N-Row">
+          <h2>N</h2>
+          {nRow.map((x) => (
+            <p className='number' id={x}>{x}</p>
+          ))}
+        </div>
+        <div id="G-Row">
+        <h2>G</h2>
+        {gRow.map((x) => (
+            <p className='number' id={x}>{x}</p>
+          ))}
+        </div>
+        <div id="O-Row">
+        <h2>O</h2>
+        {oRow.map((x) => (
+            <p className='number' id={x}>{x}</p>
+          ))}
+        </div>
       </div>
-      <div id="I-Row">
-        <h2>I</h2>
-        {iRow.map((x) => (
-          <p id="number">{x}</p>
-        ))}
-      </div>
-      <div id="N-Row">
-        <h2>N</h2>
-        {nRow.map((x) => (
-          <p id="number">{x}</p>
-        ))}
-      </div>
-      <div id="G-Row">
-      <h2>G</h2>
-      {gRow.map((x) => (
-          <p id="number">{x}</p>
-        ))}
-      </div>
-      <div id="O-Row">
-      <h2>O</h2>
-      {oRow.map((x) => (
-          <p id="number">{x}</p>
-        ))}
-      </div>
+      <button onClick={() => randomDrawing()}>Draw a number!</button>
     </div>
   )
 }
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,17 +1,19 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import BingoCard from '../components/BingoCard'
 import BingoDraw from '../components/BingoDraw'
 
 function Game() {
   const [currentDraw, setCurrentDraw] = useState("")
+  const [numbersDrawn, setNumbersDrawn] = useState([])
 
+  
 
 
   return (
     <div>
 
 
-      <BingoDraw />
+      <BingoDraw numbersDrawn={numbersDrawn} setCurrentDraw={setCurrentDraw} setNumbersDrawn={setNumbersDrawn} />
       <BingoCard />
     </div>
     
